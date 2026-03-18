600e0309967d3618e020d4d6818b00cd03fe0178
Edoardo <edoardo.di.biagio@hotmail.it>
2026-03-18 18:35:10 +0100

    commit prima di aggiornamenti corposi

---
+++ src/app/footer/footer.component.ts
@@ -0,0 +1,22 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.css'
+})
+export class FooterComponent {
+
+  isVisible_1=true;
+  isVisible_2=true;
+  isVisible_3=true;
+
+
+  text_footer_1 ="Questo è il testo del div 1"
+  text_footer_2 ="Questo è il testo del div 2"
+  text_footer_3 ="Questo è il testo del div 3"
+
+}
